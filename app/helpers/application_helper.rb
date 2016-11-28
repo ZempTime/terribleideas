e416c7158db0dabd85cfd97db7b7cde89313f2f0
Chris Zempel <zempel@efeqdev.com>
2016-11-27 19:10:01 -0600

    Adds some fancy business to label links active appropriately

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  def active?(active_class, opts={})
+    current = {}; current[:controller] = params[:controller]; current[:action] = params[:action]
+    current.keep_if {|key, value| opts.has_key?(key)}
+
+    if opts == current
+      active_class
+    else
+      ""
+    end
+  end
 end
